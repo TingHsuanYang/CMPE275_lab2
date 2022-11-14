5bcc154e3e162cecbf7e6d86c9da974b811cfb5b
TingHsuan Yang <evan.yang@sjsu.edu>
2022-11-13 23:42:19 -0800

    finish the pybind11 part

--- pybind11_test.py
+++ pybind11_test.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import pybind11_cppsort
-import numpy as np
-
 
 if __name__ == "__main__":
     # Sample data for our call:
     x = [2, 1, 4, 3, 5]
-    y = list(x)
-    a = np.array([2, 1, 4, 3, 5,4])
-    pybind11_cppsort.cpp_sort(a, len(a))
-    print(f"    In Python: array: {x} return sorted array {a}")
+
+    y = pybind11_cppsort.pybind11_cpp_sort(x)
+    print(f"    In Python: array: {x} return sorted array {y}")
     print()
--- pybind11_wrapper.cpp
+++ pybind11_wrapper.cpp
@@ -1,7 +1,43 @@
 #include <pybind11/pybind11.h>
-#include "cppsort.hpp"
+#include <pybind11/stl.h>
+
+// swap
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// partition
+int partition(std::vector<int>& array, int low, int high) {
+    int pivot = array[high];
+    int i = (low - 1);
+    for (int j = low; j < high; j++) {
+        if (array[j] <= pivot) {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    return (i + 1);
+}
+
+// quickSort
+void quickSort(std::vector<int>& array, int low, int high) {
+    if (low < high) {
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+// return a sorted vector
+std::vector<int> pybind11_cpp_sort(std::vector<int> arr) {
+    quickSort(arr, 0, arr.size() - 1);
+    return arr;
+}
 
 PYBIND11_MODULE(pybind11_cppsort, m) {
-    m.doc() = "pybind11 sort plugin"; // Optional module docstring
-    m.def("cpp_sort", &cpp_sort, "A function that sorts an array");
+    m.doc() = "pybind11 sort plugin";  // Optional module docstring
+    m.def("pybind11_cpp_sort", &pybind11_cpp_sort, "A function that sorts an array");
 }
