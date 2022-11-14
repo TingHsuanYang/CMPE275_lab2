14838b6c83c38f82e38ae794822e862b424f8a9a
Eduardo Lemus <edventura91@gmail.com>
2022-11-14 13:27:57 -0800

    added lapsed time

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import sort_Test	
+import time
 a = [1, 7, 4, 1, 10, 9, -2]
 arrlen = len(a)
-
-print(sort_Test.quickSort(a,0,arrlen-1))
-print (a)
+t = time.process_time_ns()
+sort_Test.quickSort(a,0,arrlen-1)
+elapsed_time = time.process_time_ns() - t
+print("Elapsed time (ns)")
+print (elapsed_time)
